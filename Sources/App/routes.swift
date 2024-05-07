ddc4f2f017330dbdb1cd27ad9858ab3a3291848b
briankim <brianhkim13@gmail.com>
2024-05-06 21:43:55 -0500

    procfile

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,20 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
+app.get { req -> EventLoopFuture<Response> in
+    let fileio = req.application.fileio
+    let path = req.application.directory.publicDirectory + "audio.mp3"
+    return fileio.openFile(path: path, eventLoop: req.eventLoop)
+        .flatMap { (handle, region) -> EventLoopFuture<ByteBuffer> in
+            let allocator = ByteBufferAllocator()
+            return fileio.read(fileRegion: region, allocator: allocator, eventLoop: req.eventLoop)
+        }
+        .map { buffer -> Response in
+            var headers = HTTPHeaders()
+            headers.add(name: .contentType, value: "audio/mpeg")
+            headers.add(name: .contentLength, value: buffer.readableBytes.description)
+            return Response(status: .ok, headers: headers, body: .init(buffer: buffer))
+        }
+}
 }
